e74d777ac97c343c0e3b327cdd121c73c1a6e2ee
Helder Pereira <phelder@gmail.com>
2023-07-19 20:08:14 +0100

    delete

--- code/travelBlogBE/index.js
+++ code/travelBlogBE/index.js
@@ -6,6 +6,10 @@ const destinationService = require("./services/destinationService");
 const port = 3000;
 
 app.get("/api", function(req, res) {
+    
+    // res.setHeader("content-type", "application/json");
+    // res.send(JSON.stringify("Welcome to the Travel Blog API"));
+
     res.json("Welcome to the Travel Blog API");
 });
 
@@ -20,7 +24,27 @@ app.get("/api/destinations/:destinationId", function(req, res) {
     
     const result = destinationService.getDestinationById(id);
     
-    res.json(result);
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(404);
+        res.end();
+    } 
+});
+
+app.delete("/api/destinations/:destinationId", function(req, res) {
+    const id = Number(req.params.destinationId);
+
+    const result = destinationService.deleteDestinationById(id);
+    if (result) {
+        res.json({
+            id: id,
+            message: "Deleted succesfully"
+        })
+    } else {
+        res.status(404);
+        res.end();
+    }
 });
 
 app.listen(port, function() {
--- code/travelBlogBE/services/destinationService.js
+++ code/travelBlogBE/services/destinationService.js
@@ -1,16 +1,49 @@
 const destinationData = require("./destinations.json");
 
 function getAllDestinations() {
-    return [1, 2, 3, 4, 5, 6];
+    const resultData = [];
+    for (let i = 0; i < destinationData.length; i++) {
+        const fullDestination = destinationData[i];
+        const newInfo = {
+            id: fullDestination.id,
+            title: fullDestination.titel,
+            url: "/api/destinations/" + fullDestination.id
+        }
+        resultData.push(newInfo);
+    }
+    return resultData;
 }
 
 function getDestinationById(id) {
-    return {
-        data: "you will see " + id + " here"
+    for (let i = 0; i < destinationData.length; i++) {
+        const fullDestination = destinationData[i];
+        if (fullDestination.id === id) {
+            return fullDestination;
+        }
+    }
+    return undefined;
+}
+
+function deleteDestinationById(id) {
+    let indexToDelete;
+    
+    for (let i = 0; i < destinationData.length; i++) {
+        const fullDestination = destinationData[i];
+        if (fullDestination.id === id) {
+            indexToDelete = i;        
+        }
+    }
+
+    if (indexToDelete) {
+        destinationData.splice(indexToDelete, 1);
+        return true;
+    } else {
+        return false;
     }
 }
 
 module.exports = {
     getAllDestinations,
-    getDestinationById
+    getDestinationById,
+    deleteDestinationById
 }
